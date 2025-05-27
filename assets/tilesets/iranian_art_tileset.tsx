b3be925c93f857c1a90a0d0ad7f68581eb128e41
arez007015 <arezoorahima@gmail.com>
2025-05-27 18:55:43 +0330

    Add files via upload

---
+++ assets/tilesets/iranian_art_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="iranian_art_tileset" tilewidth="64" tileheight="64" tilecount="8" columns="4">
+ <image source="iranian_art_tileset.png" width="256" height="128"/>
+</tileset>
