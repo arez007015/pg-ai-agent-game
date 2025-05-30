c5f147727f1719719e492fb7089c4fe2b23d83d9
arez007015 <arezoorahima@gmail.com>
2025-05-30 19:35:22 +0330

    Update game.js

--- docs/game.js
+++ docs/game.js
@@ -14,9 +14,9 @@ const config = {
     }
   },
   scene: {
-    preload: preload,
-    create: create,
-    update: update
+    preload,
+    create,
+    update
   }
 };
 
@@ -26,7 +26,7 @@ let resource;
 let music;
 
 function preload() {
-  this.load.image("tiles", "assets/tilesets/iranian_art_tilemap.png");
+  this.load.image("tiles", "assets/tilesets/iranian_art_tileset.png");
   this.load.tilemapTiledJSON("map", "assets/maps/iran_isometric_map.json");
   this.load.spritesheet("agent", "assets/sprites/pg_agent_goldturq.png", {
     frameWidth: 64,
@@ -41,41 +41,21 @@ function create() {
   music.play({ loop: true, volume: 0.3 });
 
   const map = this.make.tilemap({ key: "map" });
-  const tileset = map.addTilesetImage("iranian_art_tilemap", "tiles");
+  const tileset = map.addTilesetImage("iranian_art_tileset", "tiles");
   map.createLayer("Ground", tileset, 0, 0);
   map.createLayer("Structures", tileset, 0, 0);
 
-  // 🎞️ انیمیشن‌ها
-  this.anims.create({
-    key: "down",
-    frames: this.anims.generateFrameNumbers("agent", { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  this.anims.create({
-    key: "left",
-    frames: this.anims.generateFrameNumbers("agent", { start: 4, end: 7 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  this.anims.create({
-    key: "right",
-    frames: this.anims.generateFrameNumbers("agent", { start: 8, end: 11 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  this.anims.create({
-    key: "up",
-    frames: this.anims.generateFrameNumbers("agent", { start: 12, end: 15 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
   player = this.physics.add.sprite(100, 100, "agent");
   player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: "idle",
+    frames: this.anims.generateFrameNumbers("agent", { start: 0, end: 3 }),
+    frameRate: 4,
+    repeat: -1
+  });
+
+  player.anims.play("idle");
 
   cursors = this.input.keyboard.createCursorKeys();
 
@@ -91,18 +71,14 @@ function update() {
 
   if (cursors.left.isDown) {
     player.setVelocityX(-150);
-    player.anims.play("left", true);
   } else if (cursors.right.isDown) {
     player.setVelocityX(150);
-    player.anims.play("right", true);
-  } else if (cursors.up.isDown) {
+  }
+
+  if (cursors.up.isDown) {
     player.setVelocityY(-150);
-    player.anims.play("up", true);
   } else if (cursors.down.isDown) {
     player.setVelocityY(150);
-    player.anims.play("down", true);
-  } else {
-    player.anims.stop();
   }
 }
 
